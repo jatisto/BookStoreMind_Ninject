be528aaca4ffbebf2183babe3d3989dbd29300b9
jatisto <j.atisto@gmail.com>
2018-11-24 22:38:54 +0600

    AddPagingInfoClassAndCanPaginateTestOnPagingInfi
    Добавил класс PagingInfi который будет отвечать за пагинацию страниц . Так же изменил метод List в контроллере и добавил тест для проверки в проект UniTest.

--- BookStoreNinject/UnitTests/UnitTest1.cs
+++ BookStoreNinject/UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Abstract;
+using Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebUI.Controllers;
 
 namespace UnitTests
 {
@@ -7,8 +13,29 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // 
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>()
+            {
+                new Book(){BookId = 1, Name = "Book1"},
+                new Book(){BookId = 2, Name = "Book2"},
+                new Book(){BookId = 3, Name = "Book3"},
+                new Book(){BookId = 4, Name = "Book4"},
+                new Book(){BookId = 5, Name = "Book5"}
+            });
+
+            BooksController controller = new BooksController(mock.Object);
+            controller.pageSize = 3;
+
+            IEnumerable<Book> result = (IEnumerable<Book>) controller.List(2).Model;
+
+            List<Book> books = result.ToList();
+            Assert.IsTrue(books.Count == 2);
+            Assert.AreEqual(books[0].Name, "Book4");
+            Assert.AreEqual(books[1].Name, "Book5");
+
         }
     }
 }
--- BookStoreNinject/WebUI/Controllers/BooksController.cs
+++ BookStoreNinject/WebUI/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using Domain.Abstract;
 
 namespace WebUI.Controllers
@@ -10,15 +11,19 @@ namespace WebUI.Controllers
     public class BooksController : Controller
     {
         private IBookRepository repository;
+        public int pageSize = 4;
 
         public BooksController(IBookRepository repo)
         {
             this.repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Books);
+            return View(repository.Books
+                .OrderBy(book => book.BookId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize));
         }
 
     }
